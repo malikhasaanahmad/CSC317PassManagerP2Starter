8b6aabd2d5a861dfa4ca3f12827d396e0fc7ecf1
malikhasaanahmad <malikhasaanahmad@gmail.com>
2024-12-03 23:37:50 -0600

    Update PasswordModel.cs

--- Modules/Models/PasswordModel.cs
+++ Modules/Models/PasswordModel.cs
@@ -1,4 +1,13 @@
-﻿using System;
+/*
+    Author: Malik Hasaan Ahmad
+    ID: w10171527  
+    Assignment: Password Manager Part 2
+
+    Description:
+    Defines the model for storing password details.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +17,12 @@ namespace CSC317PassManagerP2Starter.Modules.Models
 {
     public class PasswordModel
     {
-       //Implement the Password Model here.
-
+        // Represents a password entry
+        public int ID { get; set; } // Unique ID for the password
+        public int UserID { get; set; } // ID of the user who owns the password
+        public string PlatformName { get; set; } // Name of the platform (e.g., Twitter)
+        public string PlatformUserName { get; set; } // Username on the platform
+        public byte[] PasswordText { get; set; } // Encrypted password
     }
 }
+
